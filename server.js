beca3fa1bf6f5ee7b8cb8deda4cbb0998d151dc2
estuchis01 <estebansoft.dev2001@gmail.com>
2025-06-01 19:55:31 +0200

    funciones integradas pafcialmente, falta la de turnos

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+
 const turnosRoutes = require('./routes/turnosRoutes');
 app.use('/turnos', turnosRoutes);
 
